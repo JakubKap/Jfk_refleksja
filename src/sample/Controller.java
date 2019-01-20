7300da05d4097d4167fe7af31518ed4c94a27dcb
Jakub Kapusta <jakubkapusta39@gmail.com>
2019-01-20 19:53:11 +0100

    Dodanie wygaszania drugiego pola w przypadku metody z 1 parametrem

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
@@ -12,6 +14,7 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -30,14 +33,26 @@ public class Controller implements Initializable {
     @FXML
     ListView listView;
 
+    @FXML
+    TextField textFieldArg1;
+
+    @FXML
+    TextField textFieldArg2;
+
+
     MethodsImport methodsImport;
+
+    List<Method> methodList;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         methodsImport = new MethodsImport();
+        methodList = new ArrayList<>();
     }
 
     public void btnOpenClicked(ActionEvent event){
+
+        if(methodList.size()>0) methodList.clear();
 
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setInitialDirectory(new File("."));
@@ -53,15 +68,25 @@ public class Controller implements Initializable {
         methodsImport.importClasses(selectedDirectory);
 
         //zapełnienie ListView nazwami metod
-        List<Method> methodList = methodsImport.getMethodList();
+        methodList = methodsImport.getMethodList();
 
         for(Method m : methodList){
             listView.getItems().add(m.toString());
-            System.out.println(m.toString());
+            //System.out.println(m.getParameterCount());
         }
 
+    }
 
+    @FXML public void handleMouseClick(MouseEvent event) {
+        /*System.out.println("clicked on " + listView.getSelectionModel().getSelectedIndex()
+                + ", on position = ");*/
+        if(methodList.get(listView.getSelectionModel().getSelectedIndex()).getParameterCount() == 1)
+            textFieldArg2.setDisable(true);
 
+            else if(methodList.get(listView.getSelectionModel().getSelectedIndex()).getParameterCount() == 2){
+                textFieldArg1.setDisable(false);
+                textFieldArg2.setDisable(false);
+            }
 
     }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Zadanie laboratoryjne z refleksji");
         primaryStage.setScene(new Scene(root, 335, 400));
         primaryStage.show();
     }
